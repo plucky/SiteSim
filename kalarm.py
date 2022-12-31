04df703ac86774a2838949e65f0fd4df5eca4bf1
Walter Fontana <walter.fontana@gmail.com>
2022-12-30 19:18:27 -0500

    stopping conditions (each observable can be a stopping condition)

--- kalarm.py
+++ kalarm.py
@@ -6,7 +6,7 @@ import sys
 import kasystem as ka
 
 
-class Watermark:
+class Watermark:  # not used for now
     def __init__(self):
         sorted_complexes = sorted(ka.system.mixture.complexes, key=lambda x: x.size, reverse=True)
         self.level = sorted_complexes[0].size
@@ -18,30 +18,36 @@ class Watermark:
         else:
             return False
 
+class Observable:
+    def __init__(self, name, value, index=0, threshold=0):
+        self.name = name
+        self.value = value  # this should be the list of deques with values
+        self.index = index
+        self.threshold = threshold
+
+    def check(self):
+        # this is the most recently observed value
+        if self.value[self.index][-1] > self.threshold:
+            return True
+        else:
+            return False
 
 class Alarm:
     """
     Manage and check stopping conditions.
     """
     def __init__(self):
-        # when scripting, external functions can be appended to self.fuse
-        self.fuse = []
-        self.watermark = Watermark()
+        self.alarm = {}
 
         self.read_alarms()
 
-        if self.watermark.threshold != 0:
-            self.fuse.append(self.watermark.check)
-
-    def check(self):
+    def trigger(self):
         """
         Check if stopping conditions are satisfied.
         """
-        for fun in self.fuse:
-            if fun():
-                if fun == self.watermark.check:
-                    s = f'{fun}'.split()[2]
-                    print(f'{s} hit threshold')
+        for alarm in self.alarm:
+            if self.alarm[alarm].check():
+                print(f'{alarm} hit threshold at time {ka.system.sim.time:.5f} (event {ka.system.sim.event})')
                 return True
         return False
 
@@ -61,6 +67,21 @@ class Alarm:
                     match = re.match(r'^%stp:\s*(.*)\s*>\s*([0-9]+)\s?', line)
                     if match:
                         name = match.group(1).strip()
-                        value = match.group(2).strip()
-                        if name == 'size_watermark':
-                            self.watermark.threshold = int(value)
+                        threshold = match.group(2).strip()
+                        if name == 'size_watermark':  # not used for now
+                            self.alarm['size_watermark'] = Watermark()
+                            self.alarm['size_watermark'].threshold = int(threshold)
+                        else:
+                            match = re.match(r'(.*)\[(.*)\]$', name)
+                            index = 0
+                            if match:
+                                _name = match.group(1)
+                                index = int(match.group(2))
+                            else:
+                                _name = name
+                            if _name in ka.system.monitor.observable_by_name:
+                                # the next() construction is because a name has a unique obs_type, so
+                                # no need to search for it.
+                                obs_type = next(iter(ka.system.monitor.observable_by_name[_name]))
+                                value = ka.system.monitor.observable_by_name[_name][obs_type]['value']
+                                self.alarm[name] = Observable(name, value, index=index, threshold=int(threshold))
