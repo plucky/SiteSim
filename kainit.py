f3b85927d638f6d8cd768587792e9565e3fd9ba2
Walter Fontana <walter.fontana@gmail.com>
2023-01-05 00:24:14 -0500

    initialization (refactored)

--- kainit.py
+++ kainit.py
@@ -17,6 +17,14 @@ import kamon
 import kalarm
 
 
+class Xargs:
+    """
+    This holds special command line arguments for special ops...
+    """
+    def __init__(self):
+        self.arg_dict = {}
+
+
 def test_invocation():
     """
     construct a commandline invocation for testing purposes
@@ -27,7 +35,10 @@ def test_invocation():
     # test_line += ['-s', 'A(p[a1.P$w a2.P a3.P], l[r.A], r[l.A]), P@100(a1[p.A], a2[p.A], a3[p.A], d[d.P])']
     # test_line += ['-s', 'A(p[a1.P$m a2.P$m a3.P$m], l[r.A$w], r[l.A]), P(a1[p.A], a2[p.A], a3[p.A], d[d.P$m])']
     # test_line += ['--sig_file', 'TestData/signature.txt']
-    test_line += ['-p', 'TestData/parameters_test2.txt']
+    test_line += ['-p', 'TestData/parameters_AP.txt']
+    test_line += ['-X', 'T', 'initial=25', 'delta=2', 'start=1', 'end=11']
+    test_line += ['-X', 'C', 'agent=A', 'initial=100', 'delta=10']
+    test_line += ['-X', 'dir', 'my/directory']
     # test_line += ['-r', 'TestOutput/report.txt']
     # test_line += ['-m', 'TestData/snap__big.ka']
     # test_line += ['-m', 'TestData/snap__0015.ka']
@@ -37,7 +48,7 @@ def test_invocation():
     return test_line
 
 
-def commandline(invocation=None):
+def parse_commandline(invocation=None):
     """
     parse the command line
 
@@ -53,18 +64,20 @@ def commandline(invocation=None):
                      help='report file name')
     cmd.add_argument('-m', '--mixture', type=str, default=None,
                      help='initial mixture file')
-    cmd.add_argument('--seed', type=int, default=-1,
+    cmd.add_argument('--seed', type=int, default=None,
                      help='random number seed')
     cmd.add_argument('-d', '--db', type=int, default=0,
                      help='reporting level')
+    cmd.add_argument('-X', '--extra', action='append', default=None, nargs='*',
+                     help='add arguments for special ops')
 
-    shortcuts = {'s': 'signature', 'r': 'report', 'p': 'parameters', 'm': 'mixture', 'd': 'db'}
+    shortcuts = {'s': 'signature', 'r': 'report', 'p': 'parameters', 'm': 'mixture', 'd': 'db', 'X': 'extra'}
 
     args = cmd.parse_args(invocation)
 
     arg_values = vars(args)  # command line {argument: value} dictionary
 
-    # reconstruct the command line used so that it can be copy/pasted and rerun
+    # reconstruct the command line used such that it can be copy/pasted and rerun
     cmdline = os.path.split(sys.argv[0])[1]  # this is the program name
     for item in sys.argv[1:]:
         if re.match('--', item):
@@ -90,20 +103,13 @@ def commandline(invocation=None):
 
     return arg_values, cmdline
 
+def commandline(invocation=None):
 
-def initialize(parameter_file=None, invocation=None, modifier_fun=None, **kwargs):
-    """
-    Initializes the system; includes commandline invocation
-    Returns: a system object
-    """
     ka.init_system()  # this creates, but does not fully initialize, the global object "system"
-
-    ka.system.parameter_file = parameter_file  # will be overridden by command line, if any
-    seed = -1  # flag to determine whether the command line contains a seed
 
     if len(sys.argv) > 1 or invocation is not None:
         # use command line or pass invocation
-        arg_values, cmdline = commandline(invocation)  # returns {argument: value} dictionary and the invocation
+        arg_values, cmdline = parse_commandline(invocation=invocation)
 
         # store the COMMAND LINE
         ka.system.cmdline = cmdline
@@ -132,68 +138,44 @@ def initialize(parameter_file=None, invocation=None, modifier_fun=None, **kwargs
 
             # random number SEED
             elif key == 'seed':
-                seed = arg_values[key]
+                ka.system.rng_seed = arg_values[key]
 
-    # create monitor (needs further initializing before starting the main loop)
-    ka.system.monitor = kamon.Monitor()
-
-    # initialize Kappa PARSER
-    ka.system.kappa = kamol.Kappa()
-
-    # initialize site-graph MATCHER
-    ka.system.sgm = kamatch.SiteGraphMatcher()
-
-    # initialize PARAMETERS. This also initializes the SIGNATURE object.
-    ka.system.parameters = kaparam.Parameters(file=ka.system.parameter_file)
-
-    # change parameters using an external function
-    if modifier_fun:
-        modifier_fun(ka.system, **kwargs)
-
-    # apply settings and compute rate constants
-    ka.system.parameters.apply_parameters()
-
-    # use the rng seed provided on the commandline, if any
-    if seed != -1:
-        ka.system.parameters.rng_seed = seed
-
-    # initialize MIXTURE, which also generates all activities
-    ka.system.mixture = kamix.Mixture(file=ka.system.mixture_file, system=ka.system)
-
-    # initialize CTMC
-    ka.system.sim = kasim.CTMC()
-
-    # initialize MONITOR
-    ka.system.monitor.initialize(file=ka.system.parameter_file)
-
-    # initialize ALARMS
-    ka.system.alarm = kalarm.Alarm()
-
-    # initial REPORT
-    ka.system.report()
-
-    # ka.system.snapshot("TestOutput/snap0.ka", label=True)
-
+            # XARGS
+            elif key == 'extra':
+                if arg_values[key]:
+                    if not ka.system.xargs:
+                        ka.system.xargs = Xargs()
+                    for item_list in arg_values[key]:
+                        ka.system.xargs.arg_dict[item_list[0]] = item_list[1:]
     return ka.system
 
+def initialize(parameter_file=None, modifier_fun=None, **kwargs):
+    """
+    Initializes the system
+    """
+    if not ka.system:
+        # this creates, but does not fully initialize, the global object "system"
+        ka.init_system()
 
-def init(parameter_file=None, modifier_fun=None, **kwargs):
-    """
-    Initializes a system without commandline and allows for parameter modifications.
-    This is useful for scripting in jupyter.
-    Returns: a system object
-    """
-    ka.init_system()  # this creates, but does not fully initialize, the global object "system"
-    ka.system.parameter_file = parameter_file
+    # if no parameter file on the command line, use supplied file name
+    if not ka.system.parameter_file:
+        ka.system.parameter_file = parameter_file
+
     # create monitor (needs further initializing before starting the main loop)
     ka.system.monitor = kamon.Monitor()
+
     # initialize Kappa PARSER
     ka.system.kappa = kamol.Kappa()
+
     # initialize site-graph MATCHER
     ka.system.sgm = kamatch.SiteGraphMatcher()
+
     # initialize PARAMETERS. This also initializes the SIGNATURE object.
     ka.system.parameters = kaparam.Parameters(file=ka.system.parameter_file)
-    # initialize MIXTURE, which also generates all activities
+
+    # command line seed overrides parameter file
+    if ka.system.rng_seed:
+        ka.system.parameters.rng_seed = ka.system.rng_seed
 
     # change parameters using an external function
     if modifier_fun:
@@ -202,13 +184,18 @@ def init(parameter_file=None, modifier_fun=None, **kwargs):
     # apply settings and compute rate constants
     ka.system.parameters.apply_parameters()
 
+    # initialize MIXTURE, which also generates all activities
     ka.system.mixture = kamix.Mixture(file=ka.system.mixture_file, system=ka.system)
+
     # initialize CTMC
     ka.system.sim = kasim.CTMC()
+
     # initialize MONITOR
     ka.system.monitor.initialize(file=ka.system.parameter_file)
+
     # initialize ALARMS
     ka.system.alarm = kalarm.Alarm()
+
     # initial REPORT
     ka.system.report()
 
@@ -224,7 +211,8 @@ def clear_SiteSim():
     del ka.system.sim
     del ka.system.monitor
     del ka.system.alarm
-    del ka.system
+    del ka.system.xargs
+    ka.system = None
 
 
 if __name__ == '__main__':
